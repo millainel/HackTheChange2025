f80e414ce98fa8843a12693cf304e623638822ed
Rajvir <rajvirz2004@gmail.com>
2025-11-08 13:49:15 -0700

    added .env file

--- mars/src/utils/mqttConfig.ts
+++ mars/src/utils/mqttConfig.ts
@@ -1,5 +1,4 @@
-// src/utils/mqttConfig.ts
-export const MQTT_URL  = "wss://fb3b51b4db3e43e9982b333f87d8b8b5.s1.eu.hivemq.cloud:8884/mqtt";
-export const MQTT_USER = "hivemq.webclient.1762633343139";
-export const MQTT_PASS = ".ji%3*0$1T2AZgYQfsRe";
-export const MQTT_TOPIC = "hack/police/esp32-01";
+export const MQTT_URL   = process.env.REACT_APP_MQTT_URL;
+export const MQTT_USER  = process.env.REACT_APP_MQTT_USER;
+export const MQTT_PASS  = process.env.REACT_APP_MQTT_PASS;
+export const MQTT_TOPIC = process.env.REACT_APP_MQTT_TOPIC;
