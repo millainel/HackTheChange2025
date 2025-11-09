73b54d94724ff816d648baa2be98588616676586
Rajvir <rajvirz2004@gmail.com>
2025-11-09 11:58:24 -0700

    everything is good

--- mars/ESP_files/wifi_connectivity.c
+++ mars/ESP_files/wifi_connectivity.c
@@ -13,7 +13,7 @@
 #define BTN_PRESSED 0
 
 int btn_pressed = 0;
-#define UNIQUE_SERIAL_ID 1684
+const id 1684
 // ---------- Wi-Fi ----------
 const char* WIFI_SSID = "bananawifi";
 const char* WIFI_PASS = "wifibox!";
@@ -127,8 +127,8 @@ void loop() {
     digitalWrite(LED_PIN, HIGH);
     char payload[256];
     snprintf(payload, sizeof(payload),
-        "{\"lat\":%.6f,\"lng\":%.6f,\"alt\":%.2f,\"sats\":%u,\"id\":%d}",
-        latest_lat, latest_long, latest_alt, satellites), UNIQUE_SERIAL_ID;
+        "{\"lat\":%.6f,\"lng\":%.6f,\"alt\":%.2f,\"sats\":%u,\"id\":%d\"lat\":%u}",
+        latest_lat, latest_long, latest_alt, satellites, id), ;
 
       bool ok = mqtt.publish(MQTT_TOPIC, payload);
       Serial.println(ok ? String("Published: ") + payload : "Publish failed");
